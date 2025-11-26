2e0f4c07e4a942d4ffb69a88012a01c8a61ee09e
Aneclecio <110948930+Aneclecio@users.noreply.github.com>
2025-11-26 18:09:39 -0300

    Update WebElementsTest.java

--- src/test/java/com/test/WebElementsTest.java
+++ src/test/java/com/test/WebElementsTest.java
@@ -1,6 +1,14 @@
+
 package com.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+import java.time.Duration;
+
+
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,55 +17,60 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class WebElementsTest {
+
+
+
+
+
+@SuppressWarnings("unused")
+public class WebElementTest {
+
 	
-	public WebDriver driver;	
+    private WebDriver driver;
 
-	@BeforeEach
-	public void setUp() throws Exception {
-		//System.setProperty("webdriver.chrome.driver", "/Users/umov.me/Dev/drivers/chromedriver");
-		System.setProperty("webdriver.geckodriver.driver", "/Users/umov.me/Dev/drivers/geckodriver");
-		
-		// Para windows
-		//System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
-		
-		//driver = new ChromeDriver();
-		driver = new FirefoxDriver();
-		driver.get("https://antoniotrindade.com.br/treinoautomacao/elementsweb.html");
-	}
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://antoniotrindade.com.br/treinoautomacao/elementsweb.html");
+    }
+    
 
-	@AfterEach
-	public void tearDown() throws Exception {
-		//Tempo para visualização
-		Thread.sleep(3000);
-		driver.quit();
-	}
+    @AfterEach
+    public void tearDown() throws Exception {
+        Thread.sleep(3000);//tempo de visualização do teste
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
-	@Test
-	public void testValidateTextField() {
-		//1. Identificar um elemento
-		WebElement tfName = driver.findElement(By.name("txtbox1"));
-		
-		//2. Fazer uma ação no elemento		
-		tfName.sendKeys("Hello world test automation");	
-		
-		//3. Validar que o texto está escrito corretamente
-		assertEquals("Hello world test automation", tfName.getAttribute("value"));		
-	}
-	
-	@Test
-	public void testValidateEnableDisableTextField() {
-		//1. Identificar um elemento na tela
-		WebElement tfEnable = driver.findElement(By.name("txtbox1"));
-		WebElement tfDisable = driver.findElement(By.name("txtbox2"));
-		
-		//2. Validar que os elementos estão habilitado e desabilitado
-		assertTrue(tfEnable.isEnabled());
-		assertFalse(tfDisable.isEnabled());			
-	}
-	
+    @Test
+    public void testValidateTextField() {
+        WebElement tfName = driver.findElement(By.name("txtbox1")); //Identificar um elemento
+        tfName.sendKeys("Hello world test automation"); //Fazer uma ação no elemento
+        assertEquals("Hello world test automation", tfName.getAttribute("value")); //Validar que o texto está escrito corretamente
+    }
+    
+
 	
 
+    @Test
+    public void testValidateEnableDisableTextField() {
+        WebElement tfEnable = driver.findElement(By.name("txtbox1"));
+        WebElement tfDisable = driver.findElement(By.name("txtbox2"));
+
+
+        assertTrue(tfEnable.isEnabled(), "Campo habilitado deveria estar ativo");
+        assertFalse(tfDisable.isEnabled(), "Campo desabilitado deveria estar inativo");
+    }
 }
+
+    
+
+		
+	
+	
+
